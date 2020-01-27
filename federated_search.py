6bf719c45b91091e51954928aead894a8e76218f
Ernesto <ernestogimeno@gmail.com>
2020-01-27 14:07:36 -0500

    iterate through a few richcontext.scholapi discovery services and check if full_text_search() method is implemented.

--- federated_search.py
+++ federated_search.py
@@ -1,15 +1,13 @@
-import codecs
+
 import sys
-import json
-import pandas as pd
-from pathlib import Path
-
+import traceback
 from richcontext import graph as rc_graph
+from richcontext import scholapi as rc_scholapi
 
 
-def load_publications (graph):#, used, out_buf, known_datasets, known_journals, known_authors, full_graph):
+def load_publications (graph):
     """
-    load publications
+    load publications. Only DOI and Title
     """
     publications = []
 
@@ -17,8 +15,6 @@ def load_publications (graph):#, used, out_buf, known_datasets, known_journals, 
         for pub in pub_iter:
             if "doi" in pub:
                 doi = pub["doi"]
-            else:
-                 doi = "TODO handle null DOI........."
             title = pub["title"]
             aPublication = dict()
             aPublication["doi"]=doi
@@ -27,27 +23,60 @@ def load_publications (graph):#, used, out_buf, known_datasets, known_journals, 
             publications.append(aPublication)
     return publications
 
-def main(terms,limit):
-    print("terms",terms)
+def api_implements_full_text_search(api):
+    """
+    Returns True if api has a method named "full_text_search".
+    Returns False if not.
+    """
+    try: #__getattribute__ raises an exception when "full_text_search" is missing
+
+        #checks if api.full_text_search is defined and is a method.
+        if callable(api.__getattribute__("full_text_search")):
+            print(api.name, "implements full_text_search")
+            return True
+    except Exception:
+        pass
+    finally:
+        print(api.name, "does NOT implement full_text_search")
+        return False
+
+def main(search_terms, limit):
+    print("terms", search_terms)
     print("limit",limit)
 
     graph = rc_graph.RCGraph("corpus")
 
     pubs = load_publications(graph)
-    print(len(pubs))
+    print(len(pubs),"publications")
+
+    known_doi = set([p["doi"] for p in pubs])
+    known_title = set([p["title"] for p in pubs])
+
+    print(len(known_doi),"known DOIs")
+    print(len(known_title),"known titles")
+
+    schol = rc_scholapi.ScholInfraAPI(config_file="rc.cfg", logger=None)
+
+    for api in [schol.openaire, schol.europepmc, schol.dimensions,schol.pubmed, schol.repec]: #TODO get a list of all APIs implemented
+        if api_implements_full_text_search(api):
+            try:
+                meta = api.full_text_search(search_term=search_terms, limit=limit)
+                print(type(meta), len(meta))
+            except Exception:
+                # debug this as an edge case
+                traceback.print_exc()
+                #print(search_terms)
+                print(api.name,'exception calling full_text_search')
+                continue
+
+            #TODO: aggregate search hits which have the same DOI or similar title.
+
+            # if meta and len(meta) > 0:
+            #     title_match = True
+            #     meta = dict(meta)
+            #     #pub[api.name] = meta
+            #     print(meta)
     return
-    # dataPath = '/bucket_final'
-    # df = pd.read_json(Path('bucket_final/20190610_usda_iri_publications.json'))
-    # df.head()
-
-    with codecs.open(Path('bucket_final/20190610_usda_iri_publications.json'), "r", encoding="utf8") as f:
-        publications = json.load(f)
-
-    extractedData = []
-    for publication in publications:
-        print("doi"+publication["doi"]+"\n")
-        extractedData.append(publication)
-    # print(publications)
 
 
 
