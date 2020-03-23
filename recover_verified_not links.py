b23cf7845b68ec2d6098f424442461c45e4a2b62
Ernesto <ernesto.gimeno@nyu.edu>
2020-03-23 15:15:58 -0400

    Created normalize_fields function to normalize 'valid' and 'dataset' columns. Created some rules to filter out datadrops (e.g. too big to be sure it was fully reviewed). Filtered out files that does not have a "dataset" column. Copied function create_pub_dict from publications_export_template.py and used it to create a "shadow" partition using the selected CSV file.

--- recover_verified_not links.py
+++ recover_verified_not links.py
@@ -4,33 +4,99 @@ import re
 from pathlib import Path
 import os
 import pandas as pd
+import json
+import numpy
 
 PATH_DATADROPS = Path("richcontextmetadata/metadata")
+PATH_DATASETS = Path("datasets")
 DEBUG = False
 
+DATADROP_SIZE_UPPER_LIMIT = 75 #Datadrops bigger than this are unlikely to be fully processed
+RATIO_LOWER_LIMIT = 0.15 # ratio defined as partition size / datadrop size. If is too low, it is probably that the reviewer did not process the whole datadrop
+
+
+def normalize_fields(datadropDF):
+    # unify different column name for valid links
+    replacements = {
+        "valid": ["valid", "valid?", "Validation", "validation", "dataset_used?","Used dataset?"],
+        "dataset": ["dataset", "datasets", "dataset_id"]
+    }
+    datadropDF.columns = datadropDF.columns.str.strip()
+    datadropDF.rename(columns={el: k for k, v in replacements.items() for el in v}, inplace=True)
+    #datadropDF["dataset"] = datadropDF["dataset"].apply(str)
+
+    return datadropDF
+
+
+#copied this function from publications_export_template.py and edited it
+def create_pub_dict(linkages_dataframe):
+
+    # Cleaning the Dataframe
+    linkages_dataframe = linkages_dataframe.loc[pd.notnull(linkages_dataframe.dataset)].drop_duplicates() #prevent null values for datasets in some datadrop files.
+    #linkages_dataframe = linkages_dataframe.loc[pd.notnull(linkages_dataframe.title)].drop_duplicates()
+
+
+    # Get Datasets Information
+    datasets_file_path = PATH_DATASETS / "datasets.json"
+    with open(datasets_file_path, encoding="utf-8") as json_file:
+        datasets = json.load(json_file)
+
+    pub_metadata_fields = ["title"]
+    original_metadata_cols = list(
+        set(linkages_dataframe.columns.values.tolist()) - set(pub_metadata_fields) - set(["dataset"]))
+
+    pub_dict_list = []
+    for i, r in linkages_dataframe.iterrows():
+        # r["title"] = scrub_unicode(r["title"])
+        ds_id_list = [f for f in [d.strip() for d in r["dataset"].split(",")] if f not in ["", " "]]
+        for ds in ds_id_list:
+            check_ds = [b for b in datasets if b["id"] == ds]
+            if len(check_ds) == 0:
+                print("dataset {} isnt listed in datasets.json. Please add to file".format(ds))
+        required_metadata = r[pub_metadata_fields].to_dict()
+        required_metadata.update({"datasets": ds_id_list})
+        pub_dict = required_metadata
+        if len(original_metadata_cols) > 0:
+            original_metadata_raw = r[original_metadata_cols].to_dict()
+            # original_metadata_raw.update({"date_added": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")})
+            original_metadata = {k: v for k, v in original_metadata_raw.items() if not pd.isnull(v)}
+            pub_dict.update({"original": original_metadata})
+        pub_dict_list.append(pub_dict)
+    return pub_dict_list
 
 def recover_verified_not_links(filename, publications):
 
     pubs_classified_as_not_valid = False
 
     try:
-        datadropDF = pd.read_csv(filename, encoding='utf-8')
+        datadropDF = pd.read_csv(filename, encoding="utf-8")
     except Exception as e:
         # debug this as an edge case
-        print('exception while trying to open',filename,str(e))
+        print("exception while trying to open",filename,str(e))
         return
 
     #print(datadropDF.head()) #TODO: describe column valid if exists
 
-    if 'valid' in datadropDF.columns:
+    if "valid" in datadropDF.columns:
         print("valid unique values:",datadropDF.valid.nunique())
     else:
         print("valid column not present")
 
     ratio = len(publications) / len(datadropDF)
     print("partition size", len(publications),"| datadrop size", len(datadropDF),\
-          "valid/total ratio", str(ratio), "process:", ratio *100 > 5 )
-    return
+          "valid/total ratio", str(ratio), "process:", ratio *100 > 12.5 and ratio *100 != 100)
+
+    #filter out datadrops
+
+    if len(datadropDF) > DATADROP_SIZE_UPPER_LIMIT: # Datadrops too big
+        return None
+    elif len(publications) == len(datadropDF): # cvs file most probably contains only valid links
+        return None
+    elif ratio < RATIO_LOWER_LIMIT:
+        return None
+    else:
+        return create_pub_dict(  normalize_fields(datadropDF))
+
 
 
 def select_datadrop_file(datadrop_directory):
@@ -39,31 +105,38 @@ def select_datadrop_file(datadrop_directory):
         #2. longest file (to skip the files that only have valid links)
         #3. file with the shortest filename (usually -but not always- the original datadrop)
 
+    ##TODO: change criteria, it is prioritizing final vs original, related to how datadropDF.valid.nunique() is evaluated
+        ##20200302_federated2_USDA_AgriculturalResourceManagementSurvey_part5_final.csv
+
     if DEBUG: #debug
         print("***********select datadrop file***********")
+        shortest_filename = None
+        datadrop_with_most_classes = None
+        max_classes = 0
+        longest_datadrop = None
+        datadrop_max_row_count = 0
 
-    shortest_filename = None
-    datadrop_with_most_classes = None
-    max_classes = 0
-    longest_datadrop = None
-    datadrop_max_row_count = 0
-
-    auxDF = pd.DataFrame(columns=['filename','filename_lenght','valid_nunique','cant_rows'])
+    auxDF = pd.DataFrame(columns=["filename","filename_lenght","valid_nunique","cant_rows"])
 
     for filename in os.listdir(datadrop_directory):
 
         if not filename.endswith(".csv"):
             continue
+        print("analyzing file",filename)
 
         try:
-            datadropDF = pd.read_csv(datadrop_directory / filename, encoding='utf-8')
+            datadropDF = pd.read_csv(datadrop_directory / filename, encoding="utf-8")
+            #print(datadropDF.head())
 
-            # unify different column name for valid links
-            replacements = {
-                'valid': ['valid', 'valid?', 'Validation', 'validation']
-            }
-            datadropDF.columns = datadropDF.columns.str.strip()
-            datadropDF.rename(columns={el: k for k, v in replacements.items() for el in v}, inplace=True)
+            datadropDF = normalize_fields(datadropDF)
+
+            if "dataset" not in datadropDF.columns:
+                print("dataset field not included in CSV file:",datadropDF.columns)
+                continue
+            elif datadropDF["dataset"].dtypes != numpy.object:
+                print("dataset field value types incorrect:", datadropDF["dataset"].dtypes)
+                continue
+
 
             if DEBUG: # deguggin
                 if "valid" not in datadropDF.columns:
@@ -76,7 +149,7 @@ def select_datadrop_file(datadrop_directory):
                     datadrop_max_row_count = len(datadropDF)
                     longest_datadrop = filename
 
-            if 'valid' in datadropDF.columns:
+            if "valid" in datadropDF.columns:
                 nunique_valid_values = datadropDF.valid.nunique()
 
                 if DEBUG:
@@ -84,19 +157,19 @@ def select_datadrop_file(datadrop_directory):
                         max_classes = datadropDF.valid.nunique()
                         datadrop_with_most_classes = filename
             else:
-                nunique_valid_values = -1
+                nunique_valid_values = 1
 
             auxDF = auxDF.append(
                 {
-                    'filename':filename,
-                    'filename_lenght':len(filename),
-                    'valid_nunique': nunique_valid_values,
-                    'cant_rows': len(datadropDF)
+                    "filename":filename,
+                    "filename_lenght":len(filename),
+                    "valid_nunique": nunique_valid_values,
+                    "cant_rows": len(datadropDF)
                 }, ignore_index=True)
 
         except Exception as e:
             # debug this as an edge case
-            print('exception while trying to open', filename, str(e))
+            print("exception while trying to open", filename, str(e))
             continue
 
 
@@ -133,20 +206,24 @@ def main():
     cant_partitions =0
     cant_dirs = 0
     cant_files = 0
+    cant_shadow_partition =0
 
     for partition_name, pub_iter in graph.iter_publications(graph.PATH_PUBLICATIONS):
         cant_partitions += 1
 
 
-        datadrop_directory = re.sub('.json$', '', partition_name)
+        datadrop_directory = re.sub(".json$", "", partition_name)
 
-        if datadrop_directory.endswith('_publications'):
-            datadrop_directory = re.sub('_publications$', '', datadrop_directory)
+        if datadrop_directory.endswith("_publications"):
+            datadrop_directory = re.sub("_publications$", "", datadrop_directory)
 
         else:
             print("not all partitions follows the name convention:",partition_name)
 
         print("partition:",datadrop_directory)
+
+        # if datadrop_directory != "20191119_LongitudinalEmployer-HouseholdDynamicsOrigin-DestinationEmploymentStatistics":
+        #     continue
 
         #check if the partition filename matches with a datadrop directory
         if (os.path.isdir(PATH_DATADROPS / datadrop_directory )):
@@ -157,30 +234,23 @@ def main():
 
 
             if datadrop_filename:
-                recover_verified_not_links(PATH_DATADROPS / datadrop_directory / datadrop_filename, pub_iter)
+                shadow_partition = recover_verified_not_links(PATH_DATADROPS / datadrop_directory / datadrop_filename, pub_iter)
                 cant_files += 1
+                if shadow_partition:
+                    cant_shadow_partition += 1
 
-            # for filename in os.listdir(PATH_DATADROPS / datadrop_directory):
-            #     #process only original files ##TODO: validate that this if captures only the original datadrop
-            #     if filename == datadrop_directory+".csv" or filename == datadrop_directory.split('_')[-1]+".csv" :
-            #         cant_files += 1
-            #         datadrop_filename = os.path.join(datadrop_directory, filename)
-            #         print(datadrop_filename)
-            #
-            #         continue
-            #     else:
-            #         continue
         else:
             print(PATH_DATADROPS / datadrop_directory, "does not exists")
 
 
-    print('existing partitions',cant_partitions)
-    print('matching directories by name',cant_dirs)
-    print('matching files by name',cant_files)
+    print("existing partitions",cant_partitions)
+    print("matching directories by name",cant_dirs)
+    print("matching files by name",cant_files)
+    print("shadow partition",cant_shadow_partition)
 
 
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     main()
