dd4461e6cb40a81aaed39431187883ee9549bc01
Ernesto <ernesto.gimeno@nyu.edu>
2020-04-06 17:21:44 -0400

    Added a constant to switch between including valid field values other than "no" in the shadow partition. Removed the ifs that skipped datadrops without "valid" field. Open only once the datadrop files Cleaned up and commented the code.  Made it less verbose except if DEBUG==True.

--- recover_verified_not links.py
+++ recover_verified_not links.py
@@ -14,9 +14,10 @@ PATH_DATASETS = Path("datasets")
 
 PATH_SHADOW_PARTITIONS = Path("not-links_partitions")
 
-DATADROP_SIZE_UPPER_LIMIT = 75 #Datadrops bigger than this are unlikely to be fully processed
+DATADROP_SIZE_UPPER_LIMIT = 75 # When datadrops are too big, it's unlikely to be fully processed. TODO: 75 is arbitrary
 RATIO_LOWER_LIMIT = 0.15 # ratio defined as partition size / datadrop size. If is too low, it is probably that the reviewer did not process the whole datadrop
 
+SKIP_MAYBES = False
 
 def normalize_fields(datadropDF):
     # unify different column name for valid links
@@ -26,13 +27,13 @@ def normalize_fields(datadropDF):
     }
     datadropDF.columns = datadropDF.columns.str.strip()
     datadropDF.rename(columns={el: k for k, v in replacements.items() for el in v}, inplace=True)
-    #datadropDF["dataset"] = datadropDF["dataset"].apply(str)
 
     return datadropDF
 
 def normalize_valid_field_values(datadropDF):
-
-    print("before replace:",datadropDF.valid.unique())
+    # there is no standard nomenclature for "valid" and "not valid" links.
+    if DEBUG:
+        print("before replace:",datadropDF.valid.unique())
 
     # convert all values of "valid" column into strings and lowercase
     datadropDF["valid"] = datadropDF.valid.astype(str).str.lower()
@@ -46,15 +47,14 @@ def normalize_valid_field_values(datadropDF):
                             "nan": "", " - ": "", " -": "",
     }},inplace=True)
 
-    print("after replace:",datadropDF.valid.unique())
+    if DEBUG:
+        print("after replace:",datadropDF.valid.unique())
 
 #copied this function from publications_export_template.py and edited it
 def create_pub_dict(linkages_dataframe):
 
     # Cleaning the Dataframe
     linkages_dataframe = linkages_dataframe.loc[pd.notnull(linkages_dataframe.dataset)].drop_duplicates() #prevent null values for datasets in some datadrop files.
-    #linkages_dataframe = linkages_dataframe.loc[pd.notnull(linkages_dataframe.title)].drop_duplicates()
-
 
     # Get Datasets Information
     datasets_file_path = PATH_DATASETS / "datasets.json"
@@ -67,7 +67,7 @@ def create_pub_dict(linkages_dataframe):
 
     pub_dict_list = []
     for i, r in linkages_dataframe.iterrows():
-        # r["title"] = scrub_unicode(r["title"])
+
         ds_id_list = [f for f in [d.strip() for d in r["dataset"].split(",")] if f not in ["", " "]]
         for ds in ds_id_list:
             check_ds = [b for b in datasets if b["id"] == ds]
@@ -78,28 +78,16 @@ def create_pub_dict(linkages_dataframe):
         pub_dict = required_metadata
         if len(original_metadata_cols) > 0:
             original_metadata_raw = r[original_metadata_cols].to_dict()
-            # original_metadata_raw.update({"date_added": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")})
             original_metadata = {k: v for k, v in original_metadata_raw.items() if not pd.isnull(v)}
             pub_dict.update({"original": original_metadata})
         pub_dict_list.append(pub_dict)
     return pub_dict_list
 
-def recover_verified_not_links(filename, valid_links):
-
-    pubs_classified_as_not_valid = False
-
-    try:
-        datadropDF = pd.read_csv(filename, encoding="utf-8")
-    except Exception as e:
-        # debug this as an edge case
-        print("exception while trying to open",filename,str(e))
-        return
-
-    #print(datadropDF.head()) #TODO: describe column valid if exists
+def recover_verified_not_links(datadropDF, valid_links):
 
     if "valid" in datadropDF.columns:
-        print("valid unique values:",datadropDF.valid.nunique())
         normalize_valid_field_values(datadropDF)
+        print("valid unique values:",datadropDF.valid.nunique())
     else:
         print("valid column not present")
 
@@ -107,14 +95,13 @@ def recover_verified_not_links(filename, valid_links):
     print("partition size", len(valid_links), "| datadrop size", len(datadropDF),\
           "valid/total ratio", str(ratio), "process:", ratio * 100 > 12.5 and ratio * 100 != 100)
 
-    #filter out datadrops
-
+    # filter out datadrops
     if len(datadropDF) > DATADROP_SIZE_UPPER_LIMIT: # Datadrops too big
         print("Datadrop too big",len(datadropDF))
         return None
-    elif len(valid_links) == len(datadropDF): # cvs file most probably contains only valid links
+    elif len(valid_links) == len(datadropDF): # cvs file contains only valid links or is incomplete
         return None
-    elif ratio < RATIO_LOWER_LIMIT:
+    elif ratio < RATIO_LOWER_LIMIT: # some big datadrops when having a low valid ratio are not fully evaluated
         return None
     else:
 
@@ -122,8 +109,19 @@ def recover_verified_not_links(filename, valid_links):
 
         shadow_partition = list()
 
+        # iterate through each link in the datadrop
         for link in datadrop_links:
             link_exists_in_partition = False
+
+            if "valid" in link["original"]:
+                # forcing not to include links marked as valid in the datadrop file analyzed
+                if link["original"]["valid"] == "yes":
+                    continue
+                # optionally forcing to include only links marked as not valid in the datadrop
+                elif SKIP_MAYBES and link["original"]["valid"] != "no":
+                    continue
+
+            # iterate through each link in the partition (valid="yes" by definition)
             for valid_link in valid_links:
                 if "doi" in link["original"] and "doi" in valid_link["original"]:
                     if link["original"]["doi"] == valid_link["original"]["doi"]:
@@ -140,15 +138,11 @@ def recover_verified_not_links(filename, valid_links):
         return shadow_partition
 
 
-
 def select_datadrop_file(datadrop_directory):
     ## criteria for selecting best CSV file:
         #1. column "valid" with the most unique values (to make sure not valids are identified)
-        #2. longest file (to skip the files that only have valid links)
+        #2. file with most rows (to skip the files that only have valid links)
         #3. file with the shortest filename (usually -but not always- the original datadrop)
-
-    ##TODO: change criteria, it is prioritizing final vs original, related to how datadropDF.valid.nunique() is evaluated
-        ##20200302_federated2_USDA_AgriculturalResourceManagementSurvey_part5_final.csv
 
     if DEBUG: #debug
         print("***********select datadrop file***********")
@@ -159,6 +153,7 @@ def select_datadrop_file(datadrop_directory):
         datadrop_max_row_count = 0
 
     auxDF = pd.DataFrame(columns=["filename","filename_lenght","valid_nunique","cant_rows"])
+    datadrops = dict()
 
     for filename in os.listdir(datadrop_directory):
 
@@ -175,22 +170,19 @@ def select_datadrop_file(datadrop_directory):
                 datadropDF = pd.read_csv(datadrop_directory / filename, encoding="utf-8")
             except UnicodeDecodeError as e:
                 # when this exception is thrown I try one other thing that would normally fail to open the files
-                print("UnicodeDecodeError exception file",filename,str(e))
+                print("handled UnicodeDecodeError exception file",filename,str(e))
                 datadropDF = pd.read_csv(datadrop_directory / filename, engine="python")
-
-            #print(datadropDF.head())
 
             datadropDF = normalize_fields(datadropDF)
 
             if "dataset" not in datadropDF.columns:
                 print("dataset field not included in CSV file:",datadropDF.columns)
                 continue
-            elif datadropDF["dataset"].dtypes != numpy.object:
+            elif datadropDF["dataset"].dtypes != numpy.object: #some files have numbers in the dataset field.
                 print("dataset field value types incorrect:", datadropDF["dataset"].dtypes)
                 continue
 
-
-            if DEBUG: # deguggin
+            if DEBUG:
                 if "valid" not in datadropDF.columns:
                     print("after column rename:", datadropDF.columns)
 
@@ -201,16 +193,8 @@ def select_datadrop_file(datadrop_directory):
                     datadrop_max_row_count = len(datadropDF)
                     longest_datadrop = filename
 
-            #testing case
-            if "valid" not in datadropDF.columns:
-                continue
-
             if "valid" in datadropDF.columns:
                 nunique_valid_values = datadropDF.valid.nunique()
-
-                # testing case
-                if nunique_valid_values < 2:
-                    continue
 
                 # if "valid" column has 1 value typically is "yes", so I change it to -1 to penalize it compared to an empty "valid" column which typically means that is the original datadrop
                 if nunique_valid_values == 1:
@@ -231,6 +215,7 @@ def select_datadrop_file(datadrop_directory):
                     "valid_nunique": nunique_valid_values,
                     "cant_rows": len(datadropDF)
                 }, ignore_index=True)
+            datadrops[filename]= datadropDF
 
         except Exception as e:
             # debug this as an edge case
@@ -246,25 +231,20 @@ def select_datadrop_file(datadrop_directory):
         print("shortest filename:",shortest_filename)
 
     if len(auxDF) > 0:
-
+        # get the most relevant datadrop filename as first element
         auxDF = auxDF.sort_values(["valid_nunique", "cant_rows","filename_lenght"], ascending=(False, False, True))
         print(auxDF[["valid_nunique", "cant_rows","filename_lenght"]].head())
-        return auxDF.iloc[0]["filename"]
+
+        print("selected file: " + str(auxDF.iloc[0]["filename"]))
+
+        # return the most relevant datadrop as a dataframe
+        return datadrops[ auxDF.iloc[0]["filename"] ]
     else:
         return None
 
 
 def main():
-    ##TODO list:
-        #process datadrop if there is a partition (since not all datadrops where reviewed).-DONE
-        #open the original datadrop (and not the final or intermediate csv).- NEW SELECTION CRITERIA DONE
-        #validate that the original datadrop has more publications than the partition
-        #filter out datadrops too big to be sure that where fully processed
-        #validate that the partition publications exists in the original datadrop
-        #be careful with the USDA ARMS datadrops
-
-
-
+    ##TODO #Note: USDA ARMS datadrops where processed using only the abstract, here there might be several false negatives
 
     graph = rc_graph.RCGraph()
     cant_partitions =0
@@ -287,21 +267,17 @@ def main():
 
         print("partition:",datadrop_directory)
 
-        # if datadrop_directory != "20191119_LongitudinalEmployer-HouseholdDynamicsOrigin-DestinationEmploymentStatistics":
-        #     continue
-
         #check if the partition filename matches with a datadrop directory
         if (os.path.isdir(PATH_DATADROPS / datadrop_directory )):
             cant_dirs += 1
 
             # select the best candidate from all CSV files in the datadrop directory
-            datadrop_filename = select_datadrop_file(PATH_DATADROPS / datadrop_directory)
-            print("selected file: "+ str(datadrop_filename))
+            datadropDF = select_datadrop_file(PATH_DATADROPS / datadrop_directory)
 
-            if datadrop_filename:
+            if datadropDF is not None:
                 cant_files += 1
                 # create a shadow partition with the verified not-links
-                shadow_partition = recover_verified_not_links(PATH_DATADROPS / datadrop_directory / datadrop_filename, valid_links)
+                shadow_partition = recover_verified_not_links(datadropDF, valid_links)
                 if shadow_partition:
                     cant_shadow_partition += 1
                     cant_not_links += len(shadow_partition)
@@ -315,14 +291,11 @@ def main():
         else:
             print(PATH_DATADROPS / datadrop_directory, "does not exists")
 
-
     print("existing partitions",cant_partitions)
     print("matching directories by name",cant_dirs)
     print("matching files by name",cant_files)
     print("shadow partition",cant_shadow_partition)
     print("not-links",cant_not_links)
-
-
 
 
 if __name__ == "__main__":
